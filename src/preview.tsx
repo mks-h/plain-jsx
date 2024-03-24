7ce8e11e3ffe1729af8c7a5c103d19e58d65d3a0
Maksym Hazevych <mhazevych@proton.me>
2024-03-24 12:27:44 +0200

    docs: Demonstrate usage of attributes

--- src/preview.tsx
+++ src/preview.tsx
@@ -2,8 +2,9 @@ import { jsxFactory } from "./index";
 
 const SayHello = (props) => (
 	<div>
-		<h3>Hello {props ? props.name : "world"}</h3>
+		<h3 class="hi">Hello {props ? props.name : "world"}</h3>
 		<p>I hope you're having a good day</p>
+		<button onclick={'alert("Hi!!!")'}>Hi!!!</button>
 	</div>
 );
 
